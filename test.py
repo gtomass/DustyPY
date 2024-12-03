f6fc4fc0123b8fb82a6c534864673fdf0b8855a2
Radron74 <radronyt@gmail.com>
2024-12-03 14:29:52 +0100

    bug in MANIFEST

--- test.py
+++ test.py
@@ -24,9 +24,10 @@ if __name__ == '__main__':
     # Composition = {'sil-draine':1}
 
     
-    density = {'density type':'RDWA', 'shell': 100}
+    # density = {'density type':'RDWA', 'shell': 1000}
+    density = None
 
-    dust = stars.Dust(DustSize={'Distribution':'MRN'},Composition = Composition, tau = 1, Sublimation = 1500, Properties='common_and_addl_grain_composite', Temperature=600, Density=density)
+    dust = stars.Dust(DustSize={'Distribution':'MRN'},Composition = Composition, tau = 1, Sublimation = 1500, Properties='common_and_addl_grain_composite', Temperature=300, Density=density)
 
     mod = stars.Model('AFGL4106', NbStar=2, Stars=[S1, S2],
                         dust=dust, distance=1470,
@@ -67,33 +68,40 @@ if __name__ == '__main__':
     Dat.set_vizier_data(table)
     Dat.restrict_data(['xdata <= 10'])
     Dat.unred_data(EBV=1.07)
+    dustyMod.change_parameter()
+    dustyMod.lunch_dusty()
+    dustyMod.make_SED(distance=1470)
+
+    dustyMod.plot_SED(xlim=(0.1,110), ylim=(.001,3000),scale='log', ax=ax, kwargs=kwargs_dusty)
+    Dat.scatter_data(ax=ax, kwargs=kwargs_data)
+    plt.show()
 
     
-    fit = DustyFit.DustyFit(dustyMod, data=Dat)
+    # fit = DustyFit.DustyFit(dustyMod, data=Dat)
 
-    Param = {'Temp1':{'theta0':3000,'minimum':2900,'maximum':3500},
-            'Temp2':{'theta0':7000,'minimum':6000,'maximum':8000},
-            'Lum1':{'theta0':1,'minimum':0,'maximum':3},
-            'Lum2':{'theta0':1,'minimum':0,'maximum':3},
-            'Opacity':{'theta0':1,'minimum':0.1,'maximum':2},
-            'Lest':{'theta0':3,'minimum':0,'maximum':7}} #Mandatory, fit the Lestimation (Luminosity = Lest*Lestimation)
+    # Param = {'Temp1':{'theta0':3000,'minimum':2900,'maximum':3500},
+    #         'Temp2':{'theta0':7000,'minimum':6000,'maximum':8000},
+    #         'Lum1':{'theta0':1,'minimum':0,'maximum':3},
+    #         'Lum2':{'theta0':1,'minimum':0,'maximum':3},
+    #         'Opacity':{'theta0':1,'minimum':0.1,'maximum':2},
+    #         'Lest':{'theta0':3,'minimum':0,'maximum':7}} #Mandatory, fit the Lestimation (Luminosity = Lest*Lestimation)
 
-    #Initialize the parameter of the MCMC
-    ParamFit = {
-                            'nsimu': 1000,         #Number of iteration (larger is better but slow due to the dusty runtime)
-                            'updatesigma': True,  #Update the sigma of the likelihood function
-                            'method': 'dram',     #Sampling method
-                            'adaptint': 50,      #Number of interates between adaptation.
-                            'verbosity': 0,       #0=No output, 1=Output
-                            'waitbar': True,      #Display a waitbar
-                        }
-    fit.set_Param(Param)
-    fit.set_ParamFit(ParamFit)
+    # #Initialize the parameter of the MCMC
+    # ParamFit = {
+    #                         'nsimu': 1000,         #Number of iteration (larger is better but slow due to the dusty runtime)
+    #                         'updatesigma': True,  #Update the sigma of the likelihood function
+    #                         'method': 'dram',     #Sampling method
+    #                         'adaptint': 50,      #Number of interates between adaptation.
+    #                         'verbosity': 0,       #0=No output, 1=Output
+    #                         'waitbar': True,      #Display a waitbar
+    #                     }
+    # fit.set_Param(Param)
+    # fit.set_ParamFit(ParamFit)
 
-    fit.lunch_fit(chi2='Chi2_modified')
-    fit.get_Fit().print_results()
-    fit.plot_stats()
-    ax.set_xscale('log')
-    fit.plot_results(xlim=(0.1,110), ylim=(.001,3000),scale='log', ax=ax, kwargs_fit=kwargs_dusty, kwargs_data=kwargs_data, save=True,
-                     unit={'x': r'Wavelength ($\mu$m)', 'y': 'Flux (Jy)'})
+    # fit.lunch_fit(chi2='Chi2_modified')
+    # fit.get_Fit().print_results()
+    # fit.plot_stats()
+    # ax.set_xscale('log')
+    # fit.plot_results(xlim=(0.1,110), ylim=(.001,3000),scale='log', ax=ax, kwargs_fit=kwargs_dusty, kwargs_data=kwargs_data, save=True,
+    #                  unit={'x': r'Wavelength ($\mu$m)', 'y': 'Flux (Jy)'})
 
