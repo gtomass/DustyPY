dc542535d4f337d215922b934d7d92fc4b113741
Radron74 <radronyt@gmail.com>
2025-04-11 15:53:49 +0200

    feat: enhance fourth_derivative and simpson_error functions to include error estimation

--- DustyPY/libs/simpson.c
+++ DustyPY/libs/simpson.c
@@ -23,34 +23,63 @@ double simpson(double x[], double y[], int n) {
 }
 
 
-// Function to estimate the fourth derivative using finite differences
-double fourth_derivative(double x[], double y[], int n) {
-    if (n < 5) {
-        printf("Not enough points to estimate the fourth derivative.\n");
+// Function to estimate the fourth derivative using a higher-order finite difference formula
+double fourth_derivative(double x[], double y[], int n, double* error) {
+    if (n < 7) {
+        printf("Not enough points to estimate the fourth derivative with higher accuracy.\n");
+        *error = 0.0;
         return 0.0;
     }
 
     double max_d4 = 0.0;
-    for (int i = 2; i < n - 2; i++) {
-        double d4 = (y[i-2] - 4 * y[i-1] + 6 * y[i] - 4 * y[i+1] + y[i+2]) / pow(x[1] - x[0], 4);
+    double max_error = 0.0;
+    double h = x[1] - x[0]; // Assuming uniform spacing
+
+    for (int i = 3; i < n - 3; i++) {
+        // Higher-order finite difference formula for the fourth derivative
+        double d4 = (-y[i-3] + 12 * y[i-2] - 39 * y[i-1] + 56 * y[i] - 39 * y[i+1] + 12 * y[i+2] - y[i+3]) / pow(h, 4);
+
+        // Error estimation: proportional to the sixth derivative (unknown, so we approximate)
+        double d4_error = fabs(d4) * pow(h, 2); // Error is O(h^2)
+
+        // Track the maximum derivative and its associated error
         if (fabs(d4) > max_d4) {
             max_d4 = fabs(d4);
         }
+        if (d4_error > max_error) {
+            max_error = d4_error;
+        }
     }
+
+    *error = max_error;
     return max_d4;
 }
 
-// Function to estimate the error of Simpson's rule
+/// Function to estimate the error of Simpson's rule
 double simpson_error(double x[], double y[], int n) {
     if (n % 2 == 0) {
         printf("Number of intervals must be odd.\n");
         return 0.0;
     }
 
-    double h = (x[n-1] - x[0]) / (n - 1);
-    double max_d4 = fourth_derivative(x, y, n);
+    double h = (x[n-1] - x[0]) / (n - 1); // Step size
+    double a = x[0];                     // Start of the interval
+    double b = x[n-1];                   // End of the interval
+    double error_der;
+
+    // Calculate the maximum fourth derivative and its error
+    double max_d4 = fourth_derivative(x, y, n, &error_der);
 
     // Error formula: E = -(b-a)^5 / (180 * n^4) * max|f^(4)(xi)|
-    double error = (pow(x[n-1] - x[0], 5) / (180 * pow(n - 1, 4))) * max_d4;
-    return error;
+    double truncation_error = fabs((pow(b - a, 5) / (180 * pow(n - 1, 4))) * max_d4);
+
+    // Propagate the error of the fourth derivative into the Simpson error
+    double propagated_error = fabs((pow(b - a, 5) / (180 * pow(n - 1, 4))) * error_der);
+
+    // Combine the truncation error and propagated error
+    double total_error = sqrt(pow(truncation_error, 2) + pow(propagated_error, 2));
+
+    printf("Truncation Error: %e, Propagated Error: %e, Total Error: %e\n", truncation_error, propagated_error, total_error);
+
+    return total_error;
 }
