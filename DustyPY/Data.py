fix: update Data class to handle units in xdata and enhance aggregate_table to include units in the returned table

DustyPY/Data.py
@@ -293,7 +293,7 @@ class Data():
         table = utils.aggregate_table(table, column='sed_filter', fct=np.mean)
 
         self._table = table
-        self._xdata = (table['sed_freq']).to(u.um, equivalencies=u.spectral()).value
+        self._xdata = (np.asarray(table['sed_freq'])*table['sed_freq'].unit).to(u.um, equivalencies=u.spectral()).value
         self._ydata = table['sed_flux'].value
         self._yerr = table['sed_eflux'].value
 

DustyPY/utils.py
@@ -860,6 +860,8 @@ def aggregate_table(table, column: str = 'sed_filter', fct: Callable = np.mean):
     Table: La table agrégée.
     """
 
+    units = [table[col].unit for col in table.colnames]
+
     grouped = table.group_by('sed_filter')
     keys = grouped.groups.keys['sed_filter']
     new_rows = []
@@ -871,7 +873,7 @@ def aggregate_table(table, column: str = 'sed_filter', fct: Callable = np.mean):
         new_rows.append((str(keys[i]), sed_freq_mean, sed_flux_mean, sed_eflux_mean, tabname_list))
 
     # Create the new table
-    return Table(rows=new_rows, names=['sed_filter','sed_freq', 'sed_flux', 'sed_eflux', 'tabname'])
+    return Table(rows=new_rows, names=['sed_filter','sed_freq', 'sed_flux', 'sed_eflux', 'tabname'], units=units)
 
     
 